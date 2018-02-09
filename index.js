7c223cb9ccd16a356f5abcd5824b07b62a9684fc
Jessica Winters <jessica@winters.website>
2018-02-09 18:20:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const reg_alphabet = 'ABCDEFGHJKLMNPRSTVWXY'
+
+getReg = startDate => {
+  const date = new Date(startDate)
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  let reg = '-1'
+
+  if (year < 1999) {
+    if (year === 1998 && month > 6) {
+      reg = 'S'
+    } else {
+      reg = reg_alphabet[year - (month > 6 ? 1983 : 1984)]
+    }
+  } else if (year < 2002) {
+    //Post 1999 and Pre-2002 registrations follow no reliable pattern
+    const laterAlphabetRange = [
+      { low: '1999-01-01', high: '1999-02-28', reg: 'S' },
+      { low: '1999-03-01', high: '1999-08-31', reg: 'T' },
+      { low: '1999-09-01', high: '2000-02-29', reg: 'V' },
+      { low: '2000-03-01', high: '2000-08-31', reg: 'W' },
+      { low: '2000-09-01', high: '2001-02-28', reg: 'X' },
+      { low: '2001-03-01', high: '2001-08-31', reg: 'Y' },
+      { low: '2001-09-01', high: '2001-12-31', reg: '51' }
+    ]
+    for (let k = 0; k < laterAlphabetRange.length; k++) {
+      const r = laterAlphabetRange[k]
+      const lowDate = new Date(r.low)
+      const highDate = new Date(r.high)
+      if (date <= highDate && date >= lowDate) {
+        reg = r.reg
+      }
+    }
+  } else if (year > 2001) {
+    let number = year - 2000
+    if (month >= 7) {
+      number += 50
+    }
+    if (month < 2) {
+      number += 49
+    }
+    reg = pad(number, 2)
+  }
+
+  return reg
+}
+
+const pad = (n, width, z) => {
+  z = z || '0'
+  n = n + ''
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n
+}
---
+++ tests.js
@@ -0,0 +1,104 @@
+require('./index')
+
+const tests = [
+  {
+    name: 'OLD PRE JULY',
+    plates: [
+      { value: '1984-01-21', expect: 'A' },
+      { value: '1985-02-21', expect: 'B' },
+      { value: '1986-03-21', expect: 'C' },
+      { value: '1987-04-21', expect: 'D' },
+      { value: '1992-05-21', expect: 'J' },
+      { value: '1997-06-21', expect: 'P' },
+      { value: '1998-07-21', expect: 'R' }
+    ]
+  },
+  {
+    name: 'OLD POST JULY',
+    plates: [
+      { value: '1984-08-21', expect: 'B' },
+      { value: '1985-09-21', expect: 'C' },
+      { value: '1986-10-21', expect: 'D' },
+      { value: '1987-11-21', expect: 'E' },
+      { value: '1992-12-21', expect: 'K' },
+      { value: '1997-11-21', expect: 'R' },
+      { value: '1998-08-21', expect: 'S' },
+      { value: '1998-10-21', expect: 'S' }
+    ]
+  },
+  {
+    name: 'OLD POST 1999',
+    plates: [
+      { value: '1999-01-21', expect: 'S' },
+      { value: '1999-03-21', expect: 'T' },
+      { value: '1999-08-21', expect: 'T' },
+      { value: '1999-09-21', expect: 'V' },
+      { value: '2000-02-21', expect: 'V' },
+      { value: '2000-03-21', expect: 'W' },
+      { value: '2000-08-21', expect: 'W' },
+      { value: '2000-10-21', expect: 'X' },
+      { value: '2001-01-21', expect: 'X' },
+      { value: '2001-03-21', expect: 'Y' },
+      { value: '2001-07-21', expect: 'Y' },
+      { value: '2001-08-21', expect: 'Y' }
+    ]
+  },
+  {
+    name: 'NEW POST 2001',
+    plates: [
+      { value: '2002-01-21', expect: '51' },
+      { value: '2002-03-21', expect: '02' },
+      { value: '2002-06-21', expect: '02' },
+      { value: '2002-08-21', expect: '52' },
+      { value: '2002-09-21', expect: '52' },
+
+      { value: '2012-01-21', expect: '61' },
+      { value: '2012-03-21', expect: '12' },
+      { value: '2012-06-21', expect: '12' },
+      { value: '2012-08-21', expect: '62' },
+      { value: '2012-09-21', expect: '62' },
+
+      { value: '2022-01-21', expect: '71' },
+      { value: '2022-03-21', expect: '22' },
+      { value: '2022-06-21', expect: '22' },
+      { value: '2022-08-21', expect: '72' },
+      { value: '2022-09-21', expect: '72' }
+    ]
+  }
+]
+
+const runTests = () => {
+  let failedTests = 0
+  let passedTests = 0
+  for (let i = 0; i < tests.length; i++) {
+    const test = tests[i]
+    console.log('Running test: ' + test.name)
+    for (let j = 0; j < test.plates.length; j++) {
+      const plate = test.plates[j]
+      const result = getReg(plate.value)
+      if (result !== plate.expect) {
+        failedTests++
+        console.log(
+          'Test failed at value: ' +
+            plate.value +
+            '. Expected ' +
+            plate.expect +
+            ' got ' +
+            result
+        )
+      } else {
+        passedTests++
+      }
+    }
+    console.log('------------')
+  }
+  if (failedTests == 0) {
+    console.log('All ' + passedTests + ' tests passed!')
+  } else {
+    console.log('Passed ' + passedTests + ' tests')
+    console.log('Failed ' + failedTests + ' tests')
+  }
+  console.log(failedTests + passedTests + ' tests total')
+}
+
+runTests()
